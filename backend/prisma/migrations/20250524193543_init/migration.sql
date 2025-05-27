2e81cb6acd2dca5ea4ff982cc0a081d82d3daf6e
joshmatthew <joshmatthewtalplacido@gmail.com>
2025-05-28 06:03:37 +0800

    changed db from sqlite to mysql

--- backend/prisma/migrations/20250524193543_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Task" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "task_title" TEXT NOT NULL,
-    "task_description" TEXT,
-    "completed" BOOLEAN NOT NULL DEFAULT false
-);
